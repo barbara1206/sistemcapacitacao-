cc64e0c04e573369e2c7901d3dc08919e9b3edae
barbara1206 <barbaragrst9@gmail.com>
2025-05-23 12:15:59 -0300

    Add files via upload

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,19 @@
+package aplicacao;
+
+import entidades.ServidorPublico;
+
+public class Programa {
+    public static void main(String[] args) {
+        ServidorPublico isabela = new ServidorPublico();
+        isabela.setNome("Isabela");
+        isabela.setCargo("Auditor");
+        isabela.setOrgao("ANVISA");
+        isabela.setLotacao("Brasilia");
+        isabela.setEmail("isabela@gmail.");
+        isabela.setNome ("joao");
+        isabela.calcularSalarioHorasExtras (horasTrabalhadas 10,valorHoras 5,40);
+
+        System.out.println("Servidor: "+ isabela.getNome());
+        System.out.print("Horas extras R$ % 2F: ", isabela.get)
+    }
+}
