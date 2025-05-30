4bc8e259304b077ef99d9a4a15af3689a7a47d76
barbara1206 <barbaragrst9@gmail.com>
2025-05-30 13:30:31 -0300

    VarArgs 3.0

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,23 +1,28 @@
 package aplicacao;
-
+import java.util.Scanner;
+import entidades.Produto
 import entidades.ServidorPublico;
+public class Programa { public static void main(String[] args) {
+//        ServidorPublico isabela = new ServidorPublico();
+//        isabela.setNome("Isabela");
+//        isabela.setCargo("Auditor");
+//        isabela.setOrgao("ANVISA");
+//        isabela.setLotacao("Brasilia");
+//        isabela.setEmail("isabela@gmail.");
+//        isabela.setNome("joao");
+//
+//        ServidorPublico joao = new ServidorPublico();
+//        ServidorPublico marcia = new ServidorPublico(123,"marcia");
+//        ServidorPublico jose =  new ServidorPublico(144, "jose", "auditor");
+//
+//
+//        isabela.setHorasExtras(600);
+//        isabela.setSalarioMensal(3000);
+//        isabela.calcularNumeros(4,10,8);
+//        System.out.println("Servidor: "+ isabela.getNome());
+//        System.out.printf("Horas extras R$ %.2f: ",isabela.getHorasExtras());
 
-public class Programa {
-    public static void main(String[] args) {
-        ServidorPublico isabela = new ServidorPublico();
-        isabela.setNome("Isabela");
-        isabela.setCargo("Auditor");
-        isabela.setOrgao("ANVISA");
-        isabela.setLotacao("Brasilia");
-        isabela.setEmail("isabela@gmail.");
-
-        isabela.setHorasExtras(600);
-        isabela.setSalarioMensal(3000);
-        isabela.calcularNumeros(4,10,8);
-        System.out.println("Servidor: "+ isabela.getNome());
-        System.out.printf("Horas extras R$ %.2f: ",isabela.getHorasExtras());
-
-
-
+   Produto produto = new Produto();
+   Scanner scr = new Scanner(System.in);
     }
 }
